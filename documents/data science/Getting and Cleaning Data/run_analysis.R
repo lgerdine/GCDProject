2cbf11bf7430a94e65d7728a504834ed12c97a18
LaurelG <laurel.gerdine@gmail.com>
2015-03-19 13:28:59 -0400

    run_analysis.R for course project

---
+++ documents/data science/Getting and Cleaning Data/run_analysis.R
@@ -0,0 +1,84 @@
+# The purpose of this R script is to: 
+## 1.Merge the training and the test sets to create one data set.
+## 2.Extract only the measurements on the mean and standard deviation for each measurement. 
+## 3.Use descriptive activity names to name the activities in the data set
+## 4.Appropriately label the data set with descriptive variable names. 
+## 5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+## created in March 2015 by Laurel Gerdine()
+
+## info on data set: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+# prep work space (set directory and load packages)
+
+setwd("C:/Users/Owner/Documents/Data Science/data")
+library(data.table)
+library(plyr)
+library(dplyr)
+
+# read in the files and merge them together to create one data set
+
+# notes: web address is -- https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+# I unzipped the files in windows. I tried to do this as part of this script but 
+# I could not get it to work. The command I tried was unzip("C:/Users/Owner/Documents/Data Science/data/getdata_projectfiles_UCI_HAR_dataset")
+
+features <- read.table("features.txt")
+
+cleanupcolumnnames <- features$V2
+cleanupcolumnnames <- gsub("-","",cleanupcolumnnames)
+cleanupcolumnnames <- gsub(",","",cleanupcolumnnames)
+cleanupcolumnnames <- gsub("(","",cleanupcolumnnames, fixed = TRUE)
+cleanupcolumnnames <- gsub(")","",cleanupcolumnnames)
+features$V2 <- cleanupcolumnnames
+featurenames <- features[,2]
+
+activity_labels <- read.table("activity_labels.txt")
+subject_test <- read.table("subject_test.txt")
+X_test <- read.table("X_test.txt")
+y_test <- read.table("y_test.txt")
+subject_train <- read.table("subject_train.txt")
+X_train <- read.table("X_train.txt")
+y_train <- read.table("y_train.txt")
+
+# replace column names with feature names (more descriptive)
+
+colnames(X_test) <- featurenames
+colnames(X_train) <- featurenames
+colnames(y_train) <- c("Activity")
+colnames(y_test) <- c("Activity")
+colnames(subject_test) <- c("Subject")
+colnames(subject_train) <- c("Subject")
+colnames(activity_labels) <- c("Activity","ActivityType")
+
+# merge files
+
+trainmerge <- cbind (subject_train, y_train, X_train)
+testmerge <- cbind(subject_test, y_test, X_test)
+wholemerge <- rbind(trainmerge,testmerge)
+dt <- wholemerge
+
+# extract columns that contain mean and std - ALL columns that contain some form of mean or std
+
+meancolumns <- dt [ , grepl("mean", names(dt), fixed = TRUE)] ## selected all columns that contained some type of mean
+stdcolumns <- dt [ , grepl("std", names(dt), fixed = TRUE)] ## selected all columns that contained some type of std
+idcolumns <- dt[,1:2]
+dt1 <- cbind(idcolumns,meancolumns,stdcolumns)
+
+# label the data set with the descriptive name variables for activities
+
+dt2 <-join(dt1, activity_labels, by = "Activity" )
+activity_type <- dt2$ActivityType
+dt2$Activity <- activity_type
+dt2 <- dt2[,-82]
+
+# create a second independent data set with the average of each variable 
+# for each activity and each subject
+
+dt3 <- dt2 %>% group_by(Subject, Activity) %>%
+  summarise_each(funs(mean, "mean", mean(., na.rm = TRUE)))
+
+head(dt3)
+
+write.table(dt3, file = "C:/Users/OWner/Documents/tidydata.csv")
+
+# done!
